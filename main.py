4a0d241dbb262f790ff7d249ea25055c1b6d8ce8
abe <149753452+Abeltodoanything@users.noreply.github.com>
2025-06-05 17:13:54 -0700

    .

--- main.py
+++ main.py
@@ -1,48 +1,18 @@
 import tkinter as tk
-from tkinter import ttk
-import customtkinter as ctk
-# import os
-# from dotenv import load_dotenv
-# from playwright.sync_api import Playwright, sync_playwright
-# from openai import OpenAI
+import ttkbootstrap as ttk
 
-# window = ctk.CTk()
-window = tk.Tk()
+window = ttk.Window(themename='simplex')
 window.title("ANIMATED-ENIGMA")
-window.geometry("500x400")
-# ctk.set_appearance_mode('dark')
-# ctk.set_default_color_theme('theme.json')
-font1 = 'Helvetica'
+window.geometry("600x700")
 
-# =TKINTER GUI========================================================================= #
+items = ['ice cream', 'pizza', 'broccoli']
+foodstring = tk.StringVar(value= items[0])
 
-# ctk.CTkLabel(window, width=500, text="Canvas quiz bot", font=(font1, 25, 'bold')).pack()
+combo = ttk.Combobox(window, textvariable=foodstring)
+combo['values'] = items
+combo.pack(pady=10)
 
-# ctk.CTkLabel(window, text="Username:", font=(font1, 15)).pack(pady=5)
-# username1 = ctk.CTkEntry(window, font=(font1, 15))
-# username1.pack()
+combo.bind('<<ComboboxSelected>>', lambda event: print(foodstring.get()))
 
-# ctk.CTkLabel(window, text="Password:", font=(font1, 15)).pack(pady=5)
-# password2 = ctk.CTkEntry(window, font=(font1, 15), show='*')
-# password2.pack()
-
-# ctk.CTkLabel(window, text="Canvas quiz link:", font=(font1, 15)).pack(pady=5)
-# link1 = ctk.CTkEntry(window, font=(font1, 15))
-# link1.pack()
-
-# ctk.CTkLabel(window, text="Number of questions in quiz:", font=(font1, 15)).pack(pady=5)
-# quiznum = ctk.CTkEntry(window, font=(font1, 15))
-# quiznum.pack()
-
-# ctk.CTkButton(window, text="Start", width=100).pack(pady=25)
-
-# ctk.CTkTextbox(window, width=400, height=200, font=('Monaco', 12), activate_scrollbars=True).pack()
-
-# #=====================================#
-title_label = tk.Label(master=window, text="Miles to kilometers", font=(font1, 24, 'bold')).pack()
-
-input_frame = tk.Frame(master=window)
-input_frame.pack()
-tk.Entry(master=input_frame).pack(side='left', pady=10)
-tk.Button(master=input_frame, text='Convert').pack(side='left',pady=10)
+# run
 window.mainloop()
